fee4eb8ca1537762ad9249e1b55ff539907b3ba4
Riyad Ait Bayaich <83143836+SPACE-lua@users.noreply.github.com>
2023-04-16 16:19:16 +0200

    dessine

--- Core/image.lua
+++ Core/image.lua
@@ -6,8 +6,11 @@ function ImgManager.new(pfile)
     img.y = 0
     img.h = 0
     img.w = 0
-    img.data = love.graphics.newImages(pfile)
-    img.w, img.h = img.data:getdimensions()
+    img.data = love.graphics.newImage(pfile)
+    img.w, img.h = img.data:getDimensions()
+    function img.draw()
+        love.graphics.draw(img.data, img.x, img.y)
+    end
     return img
 end
 
--- Game/game.lua
+++ Game/game.lua
@@ -3,12 +3,8 @@ local Game = {}
 function Game.load()
   local files = love.filesystem.getDirectoryItems("Game/Ressources")
   for k, file in ipairs(files) do
-    print(k .. ". " .. file) --outputs something like "1. main.lua"
-    Core.image.new("Game/Ressources"..file)
+    Core.image.new("Game/Ressources/"..file)
   end
---  for i=1,  do
---    Core.image.new()
---  end
 end
 
 return Game
