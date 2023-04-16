afb0388f1a3d252629f40a264d2a7fa8cf6cef63
Riyad Ait Bayaich <83143836+SPACE-lua@users.noreply.github.com>
2023-04-16 16:53:07 +0200

    prog

--- Game/game.lua
+++ Game/game.lua
@@ -5,31 +5,34 @@ Game.images = {}
 local player = {}
 player.x = 0
 player.y = 0
+player.speed = 100
+player.animation = nil
+player.timer = Core.timer.new()
+player.sequence = {}
+player.sequence.idle = {4, 5}
+player.sequence.current = "idle"
+player.current.frame = 1
 
 function Game.load()
   local files = love.filesystem.getDirectoryItems("Game/Ressources")
   for k, file in ipairs(files) do
     table.insert( Game.images, Core.image.new("Game/Ressources/"..file) )
   end
+  player.animation = Game.images[4]
+  player.y = love.graphics.getHeight() - player.animation.h
 end
 
 function Game.update(dt)
    if love.keyboard.isDown("d") then
-    player.x = player.x + 100*dt
+    player.x = player.x + player.speed*dt
   end
   if love.keyboard.isDown("q") then
-    player.x = player.x - 100*dt
-  end
-  if love.keyboard.isDown("z") then
-    player.y = player.y - 100*dt
-  end
-  if love.keyboard.isDown("s") then
-    player.y = player.y + 100*dt
+    player.x = player.x - player.speed*dt
   end
 end
 
 function Game.draw()
-  Game.images[1].draw(player.x, player.y)
+  player.animation.draw(player.x, player.y)
 end
 
 return Game
