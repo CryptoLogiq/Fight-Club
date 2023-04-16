e060589210ba8292a73956e8de4eaf3829f25ead
Riyad Ait Bayaich <83143836+SPACE-lua@users.noreply.github.com>
2023-04-16 15:58:51 +0200

    img

--- Core/image.lua
+++ Core/image.lua
@@ -1,12 +1,13 @@
 local ImgManager = {}
 
-function ImgManager.new()
+function ImgManager.new(pfile)
     local img = {}
     img.x = 0
     img.y = 0
     img.h = 0
     img.w = 0
-    img.data = love.graphics.newImages()
+    img.data = love.graphics.newImages(pfile)
+    img.data:getdimensions()
 end
 
 return ImgManager
