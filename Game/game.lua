51991d44d8df5b18729d3acab70c2fc2db11fc2f
Riyad Ait Bayaich <83143836+SPACE-lua@users.noreply.github.com>
2023-04-16 16:13:30 +0200

    img chargement

--- Game/game.lua
+++ Game/game.lua
@@ -4,6 +4,7 @@ function Game.load()
   local files = love.filesystem.getDirectoryItems("Game/Ressources")
   for k, file in ipairs(files) do
     print(k .. ". " .. file) --outputs something like "1. main.lua"
+    Core.image.new("Game/Ressources"..file)
   end
 --  for i=1,  do
 --    Core.image.new()
