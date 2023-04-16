1685a1226f1165470be78d87bec55bedfd6111ab
Crypto Logiq <orbitaldigital@gmail.com>
2023-04-16 17:07:37 +0200

    animation frames

--- Core/timer.lua
+++ Core/timer.lua
@@ -1,17 +1,21 @@
 local TimerManager = {}
+
 function TimerManager.new()
-    local timer = {}
-    timer.current = 0
-    timer.delay = 60
-    timer.speed = 60
-    function timer.update(dt)
-        timer.current =  timer.current + (timer.speed * dt)
-        if timer.current >= timer.delay then
-            timer.current = 0
-            return true
-        end
-        return false
+  local timer = {}
+  timer.current = 0
+  timer.delay = 60
+  timer.speed = 60
+  
+  function timer.update(dt)
+    timer.current =  timer.current + (timer.speed * dt)
+    if timer.current >= timer.delay then
+      timer.current = 0
+      return true
     end
-    return timer
+    return false
+  end
+  
+  return timer
 end
+
 return TimerManager
--- Game/game.lua
+++ Game/game.lua
@@ -2,37 +2,25 @@ local Game = {}
 
 Game.images = {}
 
-local player = {}
-player.x = 0
-player.y = 0
-player.speed = 100
-player.animation = nil
-player.timer = Core.timer.new()
-player.sequence = {}
-player.sequence.idle = {4, 5}
-player.sequence.current = "idle"
-player.current.frame = 1
+local player = require("Game/player")
 
 function Game.load()
   local files = love.filesystem.getDirectoryItems("Game/Ressources")
   for k, file in ipairs(files) do
     table.insert( Game.images, Core.image.new("Game/Ressources/"..file) )
   end
-  player.animation = Game.images[4]
-  player.y = love.graphics.getHeight() - player.animation.h
+  player.load()
 end
+--
 
 function Game.update(dt)
-   if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed*dt
-  end
-  if love.keyboard.isDown("q") then
-    player.x = player.x - player.speed*dt
-  end
+  player.update(dt)
 end
+--
 
 function Game.draw()
-  player.animation.draw(player.x, player.y)
+  player.draw()
 end
+--
 
 return Game
