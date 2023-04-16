1bd91c5672732b0290d0e5d196fc22e8b3d1d88a
Crypto Logiq <orbitaldigital@gmail.com>
2023-04-16 15:29:24 +0200

    main base

---
+++ main.lua
@@ -0,0 +1,21 @@
+
+
+function love.load()
+end
+--
+
+function love.update(dt)
+end
+--
+
+function love.draw()
+end
+--
+
+function love.keypressed(k,scan)
+end
+--
+
+function love.mousepressed(x,y,button)
+end
+--
