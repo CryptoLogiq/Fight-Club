738875791b1e218cf2963b17b95e440b24904400
Crypto Logiq <orbitaldigital@gmail.com>
2023-04-16 17:23:38 +0200

    args timer speed

--- Core/timer.lua
+++ Core/timer.lua
@@ -1,9 +1,9 @@
 local TimerManager = {}
-function TimerManager.new()
+function TimerManager.new(speed)
     local timer = {}
     timer.current = 0
     timer.delay = 60
-    timer.speed = 60
+    timer.speed = speed or 60
     function timer.update(dt)
         timer.current =  timer.current + (timer.speed * dt)
         if timer.current >= timer.delay then
